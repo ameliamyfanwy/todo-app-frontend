6ba005ced66f5cec01a2c1f145f735eb46943f77
Amelia Lloyd <amelialloyd31@gmail.com>
2020-01-11 13:41:45 +0000

    editing App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
             </div>
             <div className="col-12 col-md-4">
               <CatHeader name="Work" />
-              <Task color="#009999" name="2020/21 OPEX budget" />
+              <Task color="#009999" name="2020/21 budget" />
             </div>
             <div className="col-12 col-md-4">
               <CatHeader name="Life Admin" />
