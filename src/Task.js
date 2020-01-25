5fc6e1f25d3b6354510792ce371c2745b5814cc4
Amelia Lloyd <amelialloyd31@gmail.com>
2020-01-25 17:35:35 +0000

    Linking background color to completed property

--- src/Task.js
+++ src/Task.js
@@ -14,23 +14,26 @@ class Task extends React.Component {
     render() {
 
         let description;
+        let taskStyle;
 
         if (this.props.item.completed ) {
             description = <div className="col-12 col-md-9 completedTask">
-            {this.props.item.description}
-            <button button type="button" className="star">&#9733;</button>
-            </div>
+                            {this.props.item.description}
+                            <button button type="button" className="star">&#9733;</button>
+                          </div>;
+            taskStyle = { backgroundColor: "#d9d3d9"};
         } else {
             description =   <div className="col-12 col-md-9">
-        {this.props.item.description}
-        <button button type="button" className="star">&#9733;</button>
-    </div>
+                                {this.props.item.description}
+                                <button button type="button" className="star">&#9733;</button>
+                            </div>;
+            taskStyle = { backgroundColor: this.props.color };
         }
 
 
         return (
             <div>
-                <div className="row task" style={{ backgroundColor: this.props.color }}>
+                <div className="row task" style={taskStyle}>
                     {description}
                     <div className="col-4 col-md-1 px-0 editoption d-flex justify-content-center">
                         {!this.props.item.completed &&
